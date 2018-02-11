de197ee5b1a24585a8f6f4a76fd59836e15cce29
unknown <b00061382@CEN-EB2-125-15.aus.edu>
2018-02-11 16:46:14 +0400

    Feature B Added

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 
 public class HelloWorld {
+	public void addfeatureA(){
+		
+		System.out.println("Feature A added");
+	}
+	
 	public static void main(String args[])
 	{
 		System.out.println("Hello World");
